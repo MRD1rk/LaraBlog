133ea82430351a207abfe5eb5c416d560d5dd5a0
MRD1rk <mrd1rkzz@gmail.com>
2022-04-07 14:38:28 +0300

    init

--- app/Http/Requests/ParserUnionRequest.php
+++ app/Http/Requests/ParserUnionRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class BlogCategoryUpdateRequest extends FormRequest
+class ParserUnionRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
@@ -13,7 +13,6 @@ class BlogCategoryUpdateRequest extends FormRequest
      */
     public function authorize()
     {
-//        return auth()->check();
         return true;
     }
 
@@ -25,10 +24,14 @@ class BlogCategoryUpdateRequest extends FormRequest
     public function rules()
     {
         return [
-            'title' => 'required|min:5|max:200',
-            'slug' => 'max:200',
-            'description' => 'string|min:3|max:500',
-            'parent_id' => 'required|integer|exists:blog_categories,id'
+            'selected.*' => ['required','regex:/.(txt|xml|csv)/'],
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'selected.required' => 'select!'
         ];
     }
 }
